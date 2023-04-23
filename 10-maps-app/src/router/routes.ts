f060d44f32d25ea4d6713a9a1c2e31323a867968
Alejandro Oliva <alejandro.oliva2409@gmail.com>
2023-04-23 01:04:18 -0300

    vuex y vue-router added

---
+++ 10-maps-app/src/router/routes.ts
@@ -0,0 +1,9 @@
+const routes = [
+  {
+    path: '/',
+    component: () => import('../App.vue'),
+    children: [],
+  },
+];
+
+export default routes;
---
+++ 10-maps-app/src/store/index.ts
@@ -0,0 +1,9 @@
+import { createStore } from 'vuex';
+
+const store = createStore({
+  state: {
+    isSideMenuOpen: true,
+  },
+});
+
+export default store;
